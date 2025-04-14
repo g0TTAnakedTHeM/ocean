3cbe11cbe007ec2f3776af08a0f215a9cd4d01e8
g0TTAnakedTHeM <volodymyr.butko@student.uva.nl>
2025-04-14 23:32:18 +0200

    Revert language switcher implementation

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from 'react-helmet-async';
-import { LanguageProvider } from './contexts/LanguageContext';
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Navbar from './components/Navbar';
@@ -19,25 +18,23 @@ const queryClient = new QueryClient();
 const App = () => (
   <HelmetProvider>
     <QueryClientProvider client={queryClient}>
-      <LanguageProvider>
-        <TooltipProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/navbar" element={<Navbar />} />
-              <Route path="/hero" element={<Hero />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/schedule" element={<Schedule />} />
-              <Route path="/dates" element={<Dates />} />
-              <Route path="/contact" element={<Contact />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </TooltipProvider>
-      </LanguageProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/navbar" element={<Navbar />} />
+            <Route path="/hero" element={<Hero />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/schedule" element={<Schedule />} />
+            <Route path="/dates" element={<Dates />} />
+            <Route path="/contact" element={<Contact />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
     </QueryClientProvider>
   </HelmetProvider>
 );
